5c7a3ba4faf14c604ba48e588aa4182f9f633444
Givikuna <thommaster10@gmail.com>
2026-05-18 16:47:06 -0400

    skeleton

---
+++ public/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(): React.JSX.Element {
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "sans-serif" }}>
+            <h1>rgtube</h1>
+            <p>youtube in zen mode</p>
+        </div>
+    );
+}
+
+export default App;

386be6cd25b68501ed2d7b6aae98179a0eb152b2
Givikuna <thommaster10@gmail.com>
2026-05-19 20:19:52 -0400

    minor style fixes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,30 +7,35 @@ interface NavBarProps {
 
 export default function NavBar({ activeTab, setActiveTab }: NavBarProps): React.JSX.Element {
     return (
-        <nav style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "45px",
-            width: "100%",
-            backgroundColor: "#272727",
-        }}>
+        <nav
+            style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "45px",
+                width: "100%",
+                backgroundColor: "#272727",
+            }}
+        >
             <div style={{ display: "flex", gap: "6px" }}>
                 <button
                     onClick={() => setActiveTab("home")}
-                    className = {activeTab === "home" ? "nav-button.active" : "nav-button"}>
+                    className={activeTab === "home" ? "nav-button.active" : "nav-button"}
+                >
                     Home
                 </button>
-                
+
                 <button
                     onClick={() => setActiveTab("profiles")}
-                    className = {activeTab === "profiles" ? "nav-button.active" : "nav-button"}>
+                    className={activeTab === "profiles" ? "nav-button.active" : "nav-button"}
+                >
                     Profiles
                 </button>
 
                 <button
                     onClick={() => setActiveTab("channels")}
-                    className = {activeTab === "channels" ? "nav-button.active" : "nav-button"}>
+                    className={activeTab === "channels" ? "nav-button.active" : "nav-button"}
+                >
                     Channels
                 </button>
             </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home(): React.JSX.Element {
     return (
-        <div style={{textAlign: "center"}}>
+        <div style={{ textAlign: "center" }}>
             <h1>rgtube</h1>
             <p>youtube in zen mode</p>
         </div>
